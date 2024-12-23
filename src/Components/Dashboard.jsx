28c89fdf9a149f45cce63fc176b207b677eb69a1
Humayra-Adiba <tahsinadiba2024@outlook.com>
2024-12-23 13:43:54 +0600

    collected data

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import Dashboardbar from '../Common/Dashboardbar'
 
+const info = [
+    {
+        img:"disaster.svg",
+        title:"Whitechapel Rd.",
+        location:"Tulare County,  Los Angles, CA 23415",
+        amount:"$1,456,654.00"
+    },
+    {
+        img:"fighter.svg",
+        title:"Whitechapel Rd.",
+        location:"Tulare County,  Los Angles, CA 23415",
+        amount:"$1,456,654.00"
+    },
+    {
+        img:"plain.svg",
+        title:"Tulare County",
+        location:"Tulare County,  Los Angles, CA 23415",
+        amount:"$1,456,654.00"
+    },
+    {
+        img:"firefighter.svg",
+        title:"Tulare County",
+        location:"Tulare County,  Los Angles, CA 23415",
+        amount:"$1,456,654.00"
+    },
+    {
+        img:"fire.svg",
+        title:"Tulare County",
+        location:"Tulare County,  Los Angles, CA 23415",
+        amount:"$1,456,654.00"
+    },
+    {
+        img:"brokenbuilding.svg",
+        title:"Tulare County",
+        location:"Tulare County,  Los Angles, CA 23415",
+        amount:"$1,456,654.00"
+    },
+]
 function Dashboard() {
   return (
     <>
       <Dashboardbar firsttxt={"Welcome back"} secondtxt={"Dashboard"} btntxt={"Cypher AI"}/>
+
     </>
   )
 }
